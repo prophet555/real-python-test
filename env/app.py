f1f6ded427229f8e35dbfb3ec368a7f4eb409f1c
hamm44 <mrbighit@192-168-1-6.tpgi.com.au>
2014-09-15 14:14:38 +1000

    debugger

--- env/app.py
+++ env/app.py
@@ -4,15 +4,32 @@ from flask import Flask
 #create application object
 app = Flask(__name__) 
 
+#error handling
+app.config["DEBUG"] = True
+
 # use decoraters to link function to url
 @app.route("/")
 @app.route("/hello")
 
+
 # define the view using a function , which returns a string
-def hello_word():
-	return "Hello, World!"
+def hello_world():
+	return "Hello, World!?!?!!!!!!"
+
+@app.route("/test/<search_query>")
+def search(search_query):
+	return search_query
+
+@app.route("/name/<name>")
+def index(name):
+	if name.lower() == "michael":
+		return "Hello {}".format(name)
+	else:
+		return "Not Found", 404
 
 # start development server using the run() method
 if __name__ == "__main__":
 	app.run()
+
+# dynamic route
 
